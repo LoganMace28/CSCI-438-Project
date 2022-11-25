c95557d1d11dbc9526d085cff3051a035198b5a7
Pifto <lomace@iu.edu>
2022-11-25 10:33:33 -0500

    Add test for port scanner

--- node1.py
+++ node1.py
@@ -1,23 +1,23 @@
 import socket
-
+import threading
 
 #create main function
 def main():
     s = socket.socket()
 
-    port = 101
+    port = 10001
 
     nodeInfo = {"1", "u-02.sm201.iuk.edu",port, "2", "3", "1000"} 
 
-    portInfo = {"1": 101, "2": 102, "3": 103, "4": 104}
+    portInfo = {"1": 10001, "2": 10002, "3": 10003, "4": 10004}
 
     s.bind(('', port))
     print ("socket bound to %s" %(port))
 
-    s.listen(5)
-    print ("socket is listening")
+
 
     while True:
+        threading.Thread(target=listening, args=s).start()
         c, addr = s.accept()
         print ('Got connection from', addr)
         c.send('Thank you for connecting')
@@ -30,6 +30,12 @@ def updateNodeInfo(nodeInfo, portInfo):
 
 def scanPorts(portInfo):
     #Look at all available ports and see if any have changed
+    return True
+
+def listening (s):
+    #Listen for incoming connections
+    s.listen(5)
+    print ("socket is listening")
     return True
 
 main()
--- node2.py
+++ node2.py
@@ -5,20 +5,22 @@ import socket
 def main():
     s = socket.socket()
 
-    port = 102
+    port = 100002
 
     nodeInfo = {"2", "u-02.sm201.iuk.edu",port, "1", "4", "1200"} 
 
-    portInfo = {"1": 101, "2": 102, "3": 103, "4": 104}
+    portInfo = {"1": 100001, "2": 100002, "3": 100003, "4": 100004}
 
     s.bind(('', port))
     print ("socket bound to %s" %(port))
-
+    
+    s.listen(5)
+    print ("socket is listening")
 
     while True:
-        #c, addr = s.accept()
         s.connect(('127.0.0.1', portInfo["1"]))
         print (s.recv(1024).decode())
+        c, addr = s.accept()
         print ('Got connection from', addr)
         c.send('Thank you for connecting')
         c.close()
