bcda13ba7a3ae01e6e5c1a2953a24221dcd8120b
LoganMace <83689701+LoganMace28@users.noreply.github.com>
2022-12-04 14:16:09 -0500

    Update node2.1.py

--- node2.1.py
+++ node2.1.py
@@ -13,14 +13,6 @@ nodeInfo = {"2", "u-02.sm201.iuk.edu", port, "1", "4", "1200"}
 
 portInfo = {"1": 10001, "2": 10002, "3": 10001, "4": 10004}
 
-myGraph = graph.Graph(4)
-inputGraph = [[0, 1200, 0, 1200],
-             [1000, 0, 1000, 0],
-             [0, 600, 0, 600],
-             [800, 0, 800, 0]]
-myGraph.graph = inputGraph
-print("Here is the shortest distance to each node")
-myGraph.dijkstra(0)
 
 s.bind(('', port))
 print ("socket bound to %s" %(port))
