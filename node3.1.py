cc49cf5fb30a22eb0dfb3478248c603f31ff1661
LoganMace <83689701+LoganMace28@users.noreply.github.com>
2022-12-04 14:17:12 -0500

    Update node3.1.py

--- node3.1.py
+++ node3.1.py
@@ -3,7 +3,7 @@ import threading
 import graph
 import time
 
-
+# Functionally the same as all other nodes, just with node 2 info
 # create main functions
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -13,14 +13,7 @@ nodeInfo = {"3", "u-02.sm201.iuk.edu", port, "1", "4", "600"}
 
 portInfo = {"1": 10001, "2": 10001, "3": 10003, "4": 10004}
 
-myGraph = graph.Graph(4)
-inputGraph = [[0, 0, 600, 600],
-             [0, 0, 1200, 1200],
-             [1000, 1000, 0, 0],
-             [800, 800, 0, 0]]
-myGraph.graph = inputGraph
-print("Here is the shortest distance to each node")
-myGraph.dijkstra(0)
+
 
 s.bind(('', port))
 print ("socket bound to %s" %(port))
